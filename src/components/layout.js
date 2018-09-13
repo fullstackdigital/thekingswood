0d6523c33fc4a85eb47affe53768b4799a5f9f86
Tim Hingston <tim@meteor.com>
2018-09-13 22:35:02 +1000

    updated layout v2

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-
-import Header from './header'
+import kingswoodPoster from '../images/kingswood-poster.jpg'
 import './layout.css'
 
 const Layout = ({ children, data }) => (
@@ -25,12 +24,66 @@ const Layout = ({ children, data }) => (
             {
               name: 'description',
               content:
-                'natural, biodynamic & organic wine, craft beer, caffeine, kitchen, the river & you',
+                'natural, biodynamic & organic wine, tapas, craft beer, picnic boxes, caffeine, kitchen, the river & you',
             },
             {
               name: 'keywords',
               content:
                 'wine, natural wine, brunswick, brunswick heads, restaurant, food, lunch, dinner, beer, craft beer',
+            },
+            {
+              property: 'og:type',
+              content: 'website',
+            },
+            {
+              property: 'og:url',
+              content: 'https://www.thekingswood.com.au',
+            },
+            {
+              property: 'og:title',
+              content: 'The Kingswood',
+            },
+            {
+              property: 'og:description',
+              content:
+                'natural, biodynamic & organic wine, tapas, craft beer, picnic boxes, caffeine, kitchen, the river & you',
+            },
+            {
+              property: 'og:image',
+              content: kingswoodPoster,
+            },
+            {
+              property: 'og:image:width',
+              content: '512',
+            },
+            {
+              property: 'og:image:height',
+              content: '512',
+            },
+            {
+              name: 'twitter:card',
+              content: 'summary_large_image',
+            },
+            {
+              name: 'twitter:domain',
+              value: 'thekingswood.com.au',
+            },
+            {
+              name: 'twitter:creator',
+              value: '@thekingswood',
+            },
+            {
+              name: 'twitter:title',
+              value: 'The Kingswood',
+            },
+            {
+              name: 'twitter:description',
+              value:
+                'natural, biodynamic & organic wine, tapas, craft beer, picnic boxes, caffeine, kitchen, the river & you',
+            },
+            {
+              name: 'twitter:image',
+              value: kingswoodPoster,
             },
           ]}
         >
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,117 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import kingswoodPoster from '../images/kingswood-poster-square.jpg'
-
+import kingswoodBackground from '../images/shop.jpg'
+import kingswoodLogo from '../images/kingswood-logo-inverted.png'
+import instaIcon from '../images/instagram-icon-white.png'
+import fbIcon from '../images/fb-icon-white.png'
 import Layout from '../components/layout'
+import './index.css'
 
 const IndexPage = () => (
   <Layout>
+    <div className="bg-blue">
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+        }}
+      >
+        <div style={{ width: '70%' }}>
+          <img
+            src={kingswoodLogo}
+            style={{
+              marginLeft: '-12px',
+              marginBottom: '1.5rem',
+              width: '100%',
+            }}
+            alt="The Kingswood (logo)"
+          />
+          <div style={{ position: 'relative' }}>
+            <div
+              style={{
+                position: 'absolute',
+                left: '-20px',
+                borderTop: '1px solid white',
+                borderLeft: '1px solid white',
+                borderBottom: '1px solid white',
+                width: '20px',
+                height: '100%',
+              }}
+            />
+            <div
+              style={{
+                paddingTop: '20px',
+                paddingBottom: '20px',
+              }}
+            >
+              <p className="text">
+                natural, biodynamic & organic wines
+                <br />
+                tapas • craft beer • picnic boxes • caffeine
+                <br />
+                the river & you
+              </p>
+              <p className="text text--small">
+                open from midday • tapas, lunch, dinner
+                <br />6 days a week (closed tues)
+              </p>
+              <p className="text text--small">
+                2/26 Mullumbimi Street • Brunswick Heads, NSW
+                <br />
+                ph 02 6685 1111
+              </p>
+              <p>
+                <a
+                  href="https://www.instagram.com/the.kingswood/"
+                  className="text text--small"
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-start',
+                  }}
+                >
+                  <img
+                    src={instaIcon}
+                    style={{ width: '24px', marginRight: '0.5rem' }}
+                    alt="Instagram (logo)"
+                  />
+                  <span>instagram.com/the.kingswood</span>
+                </a>
+                <a
+                  href="https://www.facebook.com/thekingswoodbrunswickheads/"
+                  className="text text--small"
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-start',
+                  }}
+                >
+                  <img
+                    src={fbIcon}
+                    style={{
+                      width: '28px',
+                      marginLeft: '-2px',
+                      marginRight: '4px',
+                    }}
+                    alt="Facebook (logo)"
+                  />
+                  <span>facebook.com/thekingswoodbrunswickheads</span>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <img
-      style={{ maxHeight: '100vh', objectFit: 'contain' }}
-      src={kingswoodPoster}
+      src={kingswoodBackground}
+      style={{
+        height: '100vh',
+        width: '100%',
+        objectFit: 'cover',
+      }}
       alt="The Kingswood - Coming Soon"
     />
   </Layout>
