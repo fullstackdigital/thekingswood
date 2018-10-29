1095a3aeb4f76abdcc34c17f31543ee6ecfc4a3a
Tim Hingston <tim@meteor.com>
2018-10-28 23:02:36 -0700

    Handmade widget

--- src/pages/booking.js
+++ src/pages/booking.js
@@ -12,16 +12,20 @@ const BookingPage = () => (
     <div style={{ paddingTop: '2rem' }}>
       <h2>Make a Booking</h2>
     </div>
-    <div style={{ position: 'relative', width: '100%', minHeight: '460px' }}>
-      <div
-        className="obee-widget"
-        data-obee-url="thekingswoodbrunswickheads"
-        data-obee-widget-type="booking"
-        data-obee-min-height="450"
-        data-obee-add-height="50"
-        data-obee-scrolltop="true"
+    <div
+      style={{
+        position: 'relative',
+        display: 'flex',
+        minWidth: '520px',
+        minHeight: '460px',
+      }}
+    >
+      <iframe
+        frameBorder="0"
+        src="https://www.obee.com.au/thekingswoodbrunswickheads/"
+        width="100%"
+        style={{ minHeight: 450, backgroundColor: 'white' }}
       />
-      <script src="https://obee.com.au/common/js/dynamic/iframe.js" />
     </div>
   </HomeLayout>
 )
